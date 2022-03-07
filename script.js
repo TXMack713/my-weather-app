f236745767bcfbfb671c210c3f53d6d5b3147a88
TXMack713 <mackattack713@gmail.com>
2022-03-06 19:46:52 -0600

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello!')
+/*
+Select the weather section tag and create the DOM elements needed to display the current weather
+*/
+
+const weatherSection = document.getElementsByClassName('weather');
+
+fetch(api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid=f51a778f79966048f8772e1c2dfb9667);
