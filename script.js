4931f6e06a6d190a6a80c9da8920af756b64ce86
TXMack713 <mackattack713@gmail.com>
2022-03-07 13:14:26 -0600

    1 file modified

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ async function getLocation() {
   let x = Number.parseInt(entry);
 
   if (typeof x === Number) {
-    const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/zip?zip=${x}&appid=f51a778f79966048f8772e1c2dfb9667`
-    );
+    const url =
+      'http://api.openweathermap.org/geo/1.0/zip?zip=' +
+      x +
+      '&appid=f51a778f79966048f8772e1c2dfb9667';
+    const response = await fetch(url);
     if (response.status >= 200 && response.status <= 299) {
       const locationData = await response.json();
       console.log(locationData);
@@ -31,9 +33,11 @@ async function getLocation() {
   } else if (x === NaN) {
     let place = entry.split(',');
     if (place.length == 1) {
-      const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${place[0]}&appid=f51a778f79966048f8772e1c2dfb9667`
-      );
+      const url =
+        'http://api.openweathermap.org/geo/1.0/direct?q=' +
+        place[0] +
+        '&appid=f51a778f79966048f8772e1c2dfb9667';
+      const response = await fetch(url);
       if (response.status >= 200 && response.status <= 299) {
         const locationData = await response.json();
         console.log(locationData);
@@ -42,9 +46,13 @@ async function getLocation() {
         console.log(response.status, response.statusText);
       }
     } else if (place.length == 2) {
-      const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${place[0]},${place[1]}&appid=f51a778f79966048f8772e1c2dfb9667`
-      );
+      const url =
+        'http://api.openweathermap.org/geo/1.0/direct?q=' +
+        place[0] +
+        ',' +
+        place[1] +
+        '&appid=f51a778f79966048f8772e1c2dfb9667';
+      const response = await fetch(url);
       if (response.status >= 200 && response.status <= 299) {
         const locationData = await response.json();
         console.log(locationData);
@@ -53,9 +61,15 @@ async function getLocation() {
         console.log(response.status, response.statusText);
       }
     } else if (place.length == 3) {
-      const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${place[0]},${place[1]},${place[2]}&appid=f51a778f79966048f8772e1c2dfb9667`
-      );
+      const url =
+        'http://api.openweathermap.org/geo/1.0/direct?q=' +
+        place[0] +
+        ',' +
+        place[1] +
+        ',' +
+        place[2] +
+        '&appid=f51a778f79966048f8772e1c2dfb9667';
+      const response = await fetch(url);
       if (response.status >= 200 && response.status <= 299) {
         const locationData = await response.json();
         console.log(locationData);
@@ -74,10 +88,13 @@ async function getLocation() {
 async function getWeather(data) {
   const lat = data['lat'];
   const lon = data['lon'];
-
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=f51a778f79966048f8772e1c2dfb9667`
-  );
+  const weatherURL =
+    'https://api.openweathermap.org/data/2.5/onecall?lat=' +
+    lat +
+    '&lon=' +
+    lon +
+    '&units=imperial&appid=f51a778f79966048f8772e1c2dfb9667';
+  const response = await fetch(weatherURL);
   if (response.status >= 200 && response.status <= 299) {
     const weatherData = await response.json();
     console.log(weatherData);
