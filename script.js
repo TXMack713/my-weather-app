82bcb10e769ebe90c1f6e167137e06619e4cf570
TXMack713 <mackattack713@gmail.com>
2022-03-08 23:37:15 -0600

    2 files modified

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ submit.addEventListener('click', getLocation);
 // submit.addEventListener('click', function () {
 //   alert('You hit the submit button');
 // });
-// submit.addEventListener('click', function () {
-//   const value = document.getElementById('location').value;
-//   alert(value + ' is a typeof ' + typeof value);
-// });
+// let location = [];
+let city = '';
+let country = '';
 
 async function getLocation() {
   const entry = document.getElementById('location').value;
-  let location = entry.split(',');
+  const location = entry.split(',');
   for (let i = 0; i < location.length; i++) {
     location[i] = location[i].trim();
     console.log(location[i]);
@@ -23,20 +22,15 @@ async function getLocation() {
 
   if (x === false) {
     console.log('Number statement');
-    const url = `http://api.openweathermap.org/geo/1.0/zip?zip=${location[0]}&appid=f51a778f79966048f8772e1c2dfb9667`;
+    const url = `https://api.openweathermap.org/geo/1.0/zip?zip=${location[0]}&appid=f51a778f79966048f8772e1c2dfb9667`;
     const response = await fetch(url);
-    const data = await response.json();
+    const locationData = await response.json();
+    city = locationData['name'];
+    country = locationData['country'];
+    console.log(locationData);
+    getZipWeather(locationData);
 
-    // const lat = data['lat'];
-    // const lon = data['lon'];
-    // console.log(lat);
-    // console.log(lon);
-    // const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=f51a778f79966048f8772e1c2dfb9667`;
-    // const reply = await fetch(weatherURL);
-    // const weatherData = await reply.json();
-    // console.log(weatherData);
-    // populateWeather(weatherData);
-
+    /*
     if (response.status >= 200 && response.status <= 299) {
       const locationData = await response.json();
       console.log(locationData);
@@ -46,21 +40,25 @@ async function getLocation() {
       console.log('The problem is in the zip code api.');
       console.log(response.status, response.statusText);
     }
+     */
   } else if (x === true) {
     console.log(location);
     console.log(location.length);
     console.log('Else statement');
 
-    if (location.length == 2) {
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location[0]},${location[1]}&appid=f51a778f79966048f8772e1c2dfb9667`;
+    if (location.length === 2) {
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${location[0]},${location[1]}&appid=f51a778f79966048f8772e1c2dfb9667`;
       console.log(location[0] + ', ' + location[1]);
+      city = location[0];
+      country = location[1];
       console.log(url);
 
       const response = await fetch(url);
       const locationData = await response.json();
       console.log(locationData);
-      getWeather(locationData);
+      getTextWeather(locationData);
 
+      /*
       if (response.status >= 200 && response.status <= 299) {
         const locationData = await response.json();
         console.log(locationData);
@@ -69,27 +67,57 @@ async function getLocation() {
         // Handle errors
         console.log(response.status, response.statusText);
       }
-    } else if (location.length == 3) {
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${location[0]},${location[1]},${location[2]}&appid=f51a778f79966048f8772e1c2dfb9667`;
+       */
+    } else if (location.length === 3) {
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${location[0]},${location[1]},${location[2]}&appid=f51a778f79966048f8772e1c2dfb9667`;
       console.log(url);
+      city = location[0];
+      country = location[2];
       const response = await fetch(url);
       const locationData = await response.json();
       console.log(locationData);
-      getWeather(locationData);
+      getTextWeather(locationData);
 
-      if (response.status >= 200 && response.status <= 299) {
+      /* if (response.status >= 200 && response.status <= 299) {
         const locationData = await response.json();
         console.log(locationData);
         getWeather(locationData);
       } else {
         // Handle errors
         console.log(response.status, response.statusText);
-      }
+      } */
     }
   }
 }
 
-async function getWeather(data) {
+async function getTextWeather(data) {
+  console.log("Here's the data being passed to the getWeather method");
+  console.log(data);
+
+  const lat = data[0]['lat'];
+  const lon = data[0]['lon'];
+  console.log(lat);
+  console.log(lon);
+  const weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=f51a778f79966048f8772e1c2dfb9667`;
+  const response = await fetch(weatherURL);
+  const weatherData = await response.json();
+  console.log(weatherData);
+  populateWeather(weatherData);
+  /*
+  if (response.status >= 200 && response.status <= 299) {
+    const weatherData = await response.json();
+    console.log(weatherData);
+    populateWeather(weatherData);
+  } else {
+    // Handle errors
+    console.log('Error is in the getWeather method');
+    console.log(response.status, response.statusText);
+  }
+   */
+  // console.log(weatherData);
+}
+
+async function getZipWeather(data) {
   console.log("Here's the data being passed to the getWeather method");
   console.log(data);
 
@@ -116,33 +144,36 @@ async function getWeather(data) {
 
 function populateWeather(data) {
   //Retrieve the desired weather data to present
-  const temp = data['current']['temp'];
-  const feelsLike = data['current']['feels_like'];
-  const timezone = data['timezone'];
-  const description = data['weather']['description'];
-  const highTemp = data['daily'][0]['temp']['max'];
-  const lowTemp = data['daily'][0]['temp']['min'];
-  const windSpeed = data['daily']['wind_speed'];
-  const weatherAlert = data['alerts'][0]['description'];
-  const weatherEvent = data['alerts'][0]['event'];
+  const temp = Math.round(data['current']['temp']);
+  // const feelsLike = data['current']['feels_like'];
+  // const timezone = data['timezone'];
+  const description = data['current']['weather'][0]['description'];
+  const highTemp = Math.round(data['daily'][0]['temp']['max']);
+  const lowTemp = Math.round(data['daily'][0]['temp']['min']);
+  const windSpeed = Math.round(data['current']['wind_speed']);
+  // const windGust = Math.round(data['current']['wind_gust']);
+  // const weatherAlert = data['alerts'][0]['description'];
+  // const weatherEvent = data['alerts'][0]['event'];
 
   // Populate the screen with the current weather information
-  const weatherSection = document.getElementsByClassName('weather');
+  const weatherSection = document.getElementById('weather_display');
 
   const locale = document.createElement('h2');
-  // locale.textContent = city + ', ' + country;
+  locale.textContent = city + ', ' + country;
 
   const current = document.createElement('p');
-  current.textContent = 'Current temp: ' + temp;
+  current.textContent = 'Current temp: ' + temp + '°F';
 
   const condition = document.createElement('p');
-  condition.textContent = description;
+  condition.textContent = 'Current condition: ' + description;
 
   const range = document.createElement('p');
-  range.textContent = 'High: ' + highTemp + '   Low: ' + lowTemp;
+  range.textContent = 'High: ' + highTemp + '°F       Low: ' + lowTemp + '°F';
 
   const winds = document.createElement('p');
-  winds.textContent = windSpeed + ' MPH';
+  winds.textContent = 'Winds currently at ' + windSpeed + ' MPH';
+
+  /*
 
   const currentAdvisory = document.createElement('p');
   currentAdvisory.textContent = 'Current weather advisory: ' + weatherEvent;
@@ -150,11 +181,13 @@ function populateWeather(data) {
   const advisoryDescription = document.createElement('p');
   advisoryDescription.textContent = weatherAlert;
 
-  // weatherSection.appendChild(locale);
+  */
+  weatherSection.textContent = '';
+  weatherSection.appendChild(locale);
   weatherSection.appendChild(current);
   weatherSection.appendChild(condition);
   weatherSection.appendChild(range);
   weatherSection.appendChild(winds);
-  weatherSection.appendChild(currentAdvisory);
-  weatherSection.appendChild(advisoryDescription);
+  // weatherSection.appendChild(currentAdvisory);
+  // weatherSection.appendChild(advisoryDescription);
 }
